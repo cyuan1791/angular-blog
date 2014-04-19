615818df4fe412ee9c8d745c5536b2f96239fb48
Chih Cheng Yuan <cyuan123@live.com>
2014-04-19 10:07:18 -0700

    add blog controller and data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,3 +9,12 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }]);
+
+
+var blogApp = angular.module('blogApp', []);
+
+blogApp.controller('blogCtrl', ['$scope', '$http', function ($scop, $http) {
+    $http.get('data/blogs.json').success(function(data) {
+        $scope.blogs = data;
+    });
+}]);
