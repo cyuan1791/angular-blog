a7608d7bb5d72b941666d1920126231d77dd2aa3
Chih Cheng Yuan <cyuan123@live.com>
2014-04-19 17:15:30 -0700

    Keep

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,23 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+}])
+.controller('MyCtrl2', [function() {
 
-  }]);
+}]);
 
 
 var blogApp = angular.module('blogApp', []);
 
-blogApp.controller('blogCtrl', ['$scope', '$http', function ($scop, $http) {
+blogApp.controller('blogCtrl', ['$scope', '$http','$filter', function ($scope, $http,$filter) {
     $http.get('data/blogs.json').success(function(data) {
         $scope.blogs = data;
     });
+    $scope.selectedAlbumSongs = [ { 'name': 'song1', 'url': 'http://test/song1.mp3' }, { 'name': 'song2', 'url': 'http://test/song2.mp3' }, {'name': 'song3', 'url': 'http://test/song3.mp3' }];
+
+    $scope.selectedSongs = function () {
+        $scope.playList = $filter('filter')($scope.selectedAlbumSongs, {checked: true});
+    }
 }]);
